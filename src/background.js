66c1c3520fd0c7269f9f4394f8f4c33dd5a58a0e
Ahmed Mouelhi <100707670+amddevmh@users.noreply.github.com>
2023-09-14 12:18:56 +0200

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url.startsWith("https://chat.openai.com/")) {
-    chrome.tabs.executeScript(tabId, { file: "src/content.js" });
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ["src/content.js"]
+    });
   }
 });
